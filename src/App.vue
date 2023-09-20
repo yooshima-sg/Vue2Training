fca74265b271a5a3206cf3080d56dcfcccec4342
Ohshima Yusuke <ohshima.yusuke@yukke.org>
2023-09-20 14:48:51 +0000

    回答

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
   <section class="todoapp">
-    <!-- タイトル -->
     <header class="header">
       <h1>todos</h1>
       <input class="new-todo" autofocus autocomplete="off" placeholder="終わらせたいタスクを入力" v-model="newTodo" @keyup.enter="addTodo"/>
     </header>
 
     <main class="main" v-show="todos.length" v-cloak>
-      <!-- タイトル -->
-      <label for="toggle-all">
-        <input id="toggle-all" class="toggle-all" type="checkbox" v-model="allDone"/>
-      </label>
-      <!-- タスク一覧 -->
+      <input id="toggle-all" class="toggle-all" type="checkbox" v-model="allDone"/>
+      <label for="toggle-all"></label>
       <ul class="todo-list">
-        <li class="todo" v-for="todo in todos" :key="todo.id" :class="{ completed: todo.completed, editing: todo == editedTodo }">
+        <li class="todo" v-for="todo in filteredTodos" :key="todo.id" :class="{ completed: todo.completed, editing: todo == editedTodo }">
           <div class="view">
             <input class="toggle" type="checkbox" v-model="todo.completed" />
             <label @dblclick="editTodo(todo)">{{ todo.title }}</label>
@@ -25,41 +21,57 @@
     </main>
 
     <footer class="footer" v-show="todos.length" v-cloak>
-      <!--
-        TODO件数、フィルタボタン配置予定個所
-      -->
+      <span class="todo-count">残り <strong>{{ remaining }}</strong> 個</span>
+      <ul class="filters">
+        <li>
+          <a href="#/all" :class="{ selected: visibility.value === 'all' }">すべて</a>
+        </li>
+        <li>
+          <a href="#/active" :class="{ selected: visibility.value === 'active' }">実施中</a>
+        </li>
+        <li>
+          <a href="#/completed" :class="{ selected: visibility.value === 'completed' }">完了済</a>
+        </li>
+      </ul>
       <button class="clear-completed" @click="removeCompleted" v-show="todos.length > remaining">完了済みを削除する</button>
     </footer>
   </section>
 </template>
 
 <script>
-// import todoStorage from '@/storage.js';
+import todoStorage from '@/storage.js';
 import filters from '@/filters.js';
 import extern from '@/extern.js';
-
-let uniq_id = 0;
 
 export default {
   name: 'App',
 
   components: {
-    /* 今回は未使用 */
   },
-
+  
   data() {
     return {
-      todos: [],
+      todos: todoStorage.fetch(),
       newTodo: "",
       editedTodo: null,
       visibility: extern.visibility,
     };
   },
-
+  // data の todos を監視して、変更があったら localStrage へ todos を保存する
   watch: {
+    todos: {
+      handler(todos) {
+        todoStorage.save(todos);
+      },
+      deep: true
+    }
   },
-
+  // 計算済プロパティ
+  // http://v2.vuejs.org/guide/computed.html
   computed: {
+    filteredTodos() {
+      return filters[this.visibility.value](this.todos);
+    },
     remaining() {
       return filters.active(this.todos).length;
     },
@@ -84,7 +96,7 @@ export default {
         return;
       }
       this.todos.push({
-        id: uniq_id++,
+        id: todoStorage.uid++,
         title: value,
         completed: false
       });
@@ -92,7 +104,7 @@ export default {
     },
 
     removeTodo(todo) {
-      this.todos = this.todos.filter((t) => t.id !== todo.id);
+      this.todos.splice(this.todos.indexOf(todo), 1);
     },
 
     editTodo(todo) {
