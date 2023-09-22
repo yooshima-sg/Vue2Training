799bd918085042071d4404b55a33cea26092bc6d
Ohshima Yusuke <ooshima@s-giken.com>
2023-09-22 01:04:32 +0000

    コメント追加

--- src/storage.js
+++ src/storage.js
@@ -7,14 +7,19 @@ const STORAGE_KEY = "todos-vuejs-2.0";
 
 const storage = {
   uid: 0,
+  
+  // 保管されているTODOデータをすべて取得する
   fetch() {
     const todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
-    todos.forEach((todo, index) => {
-      todo.id = index;
-    });
-    this.uid = todos.length;
+
+    // 新たにidを振るため、起点となる数値として、
+    // 保存されているデータの最大id + 1を計算する
+    this.uid = Math.max(0, ...todos.map(t => t.id + 1));
+
     return todos;
   },
+
+  // アプリ上のTODOデータを保管する
   save(todos) {
     localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
   }
